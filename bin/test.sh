95ebd7754cd1745f718cc90f400972b8b8d3f21d
Markus Zapke-Gründemann <markus@transcode.de>
2018-01-02 03:19:23 +0100

    Revised code for POSIX sh

--- bin/test.sh
+++ bin/test.sh
@@ -6,25 +6,11 @@
 # Usage:
 #
 #  ./bin/test.sh [TAG]
-#
-# Based on a template by BASH3 Boilerplate v2.3.0
-# http://bash3boilerplate.sh/#authors
-#
-# The MIT License (MIT)
-# Copyright (c) 2013 Kevin van Zonneveld and contributors
-# You are not obligated to bundle the LICENSE file with your b3bp projects as long
-# as you leave these references intact in the header comments of your source files.
 
 # Exit on error. Append "|| true" if you expect an error.
-set -o errexit
-# Exit on error inside any functions or subshells.
-set -o errtrace
-# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
-set -o nounset
+set -e
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 set -o pipefail
-# Turn on traces, useful while debugging but commented out by default
-# set -o xtrace
 
 image="sphinx-doc"
 tag=$*
