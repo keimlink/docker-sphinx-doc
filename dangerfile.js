ec4e9317dd5f2867c3fc0166f717ecef01dcb167
Markus Zapke-Gründemann <markus@transcode.de>
2018-02-09 21:59:58 +0100

    dangerfile: Remove code to warn about updated Docker digest
    Because both images have a different base this makes no sense. We have to wait
    until Renovate supports multiple Dockerfiles.

--- dangerfile.js
+++ dangerfile.js
@@ -10,14 +10,6 @@ if (!pr.body || pr.body.length < 10) {
     fail('Please add a description to your Pull Request.');
 }
 
-// Warn if digest of Dockerfile has been updated and point out files that need to be updated too.
-if (modifiedFiles.includes('Dockerfile')) {
-    const title = 'Dockerfile digest update';
-    const filesWithVersion = danger.github.utils.fileLinks(['Dockerfile.latex']);
-    const idea = `Please update ${filesWithVersion} before merging.`;
-    warn(`${title} - <i>${idea}</i>`);
-}
-
 // Warn if version has been updated and point out files that need to be updated too.
 if (modifiedFiles.includes('requirements.pip')) {
     const title = 'Version update';
