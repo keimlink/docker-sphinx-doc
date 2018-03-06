e4a3fbc0d1c5a7c58552b62f821f6dec1540ce0b
Markus Zapke-Gründemann <markus@transcode.de>
2018-03-06 12:35:50 +0100

    Update shell script indentation to four spaces

--- bin/image.sh
+++ bin/image.sh
@@ -18,53 +18,53 @@ readonly IMAGE=$2
 readonly TAG=$3
 readonly USAGE="Usage: $(basename "$0") [COMMAND] [IMAGE] [TAG]
 
-  Available commands:
-    build
-    push
-    save
-    test
+    Available commands:
+        build
+        push
+        save
+        test
 "
 readonly VCS_REF=$(git rev-parse --short HEAD)
 readonly VERSION=$(grep ^sphinx== requirements.pip | tr -s '==' | cut -d '=' -f 2)
 
 image_build()
 {
-  case "${TAG}" in
-    latest)
-      docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
-        --build-arg VCS_REF="${VCS_REF}" \
-        --build-arg VERSION="${VERSION}" \
-        --tag "${IMAGE}:${VERSION}" \
-        --tag "${IMAGE}:${TAG}" \
-        .
-      ;;
-    latex)
-      docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
-        --build-arg VCS_REF="${VCS_REF}" \
-        --build-arg VERSION="${VERSION}" \
-        --file Dockerfile."${TAG}" \
-        --tag "${IMAGE}:${VERSION}-${TAG}" \
-        --tag "${IMAGE}:${TAG}" \
-        .
-      ;;
-  esac
+    case "${TAG}" in
+        latest)
+            docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
+                --build-arg VCS_REF="${VCS_REF}" \
+                --build-arg VERSION="${VERSION}" \
+                --tag "${IMAGE}:${VERSION}" \
+                --tag "${IMAGE}:${TAG}" \
+                .
+            ;;
+        latex)
+            docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
+                --build-arg VCS_REF="${VCS_REF}" \
+                --build-arg VERSION="${VERSION}" \
+                --file Dockerfile."${TAG}" \
+                --tag "${IMAGE}:${VERSION}-${TAG}" \
+                --tag "${IMAGE}:${TAG}" \
+                .
+            ;;
+    esac
 }
 
 image_push()
 {
-  case "${TAG}" in
-    latest) docker push "${IMAGE}:${VERSION}" ;;
-    latex) docker push "${IMAGE}:${VERSION}-${TAG}" ;;
-  esac
-  docker push "${IMAGE}:${TAG}"
+    case "${TAG}" in
+        latest) docker push "${IMAGE}:${VERSION}" ;;
+        latex) docker push "${IMAGE}:${VERSION}-${TAG}" ;;
+    esac
+    docker push "${IMAGE}:${TAG}"
 }
 
 image_save()
 {
-  case "${TAG}" in
-    latest) docker save "${IMAGE}:${VERSION}" "${IMAGE}:${TAG}" ;;
-    latex) docker save "${IMAGE}:${VERSION}-${TAG}" "${IMAGE}:${TAG}" ;;
-  esac
+    case "${TAG}" in
+        latest) docker save "${IMAGE}:${VERSION}" "${IMAGE}:${TAG}" ;;
+        latex) docker save "${IMAGE}:${VERSION}-${TAG}" "${IMAGE}:${TAG}" ;;
+    esac
 }
 
 image_test_cmd()
@@ -82,20 +82,20 @@ image_test()
     image_test_cmd "make --directory=docs html"
     [ -f docs/conf.py ] && [ -f docs/Makefile ] && [ -f docs/_build/html/index.html ]
     if [ "${TAG}" = "latex" ]; then
-      image_test_cmd "make --directory=docs latexpdf LATEXMKOPTS='-silent'"
-      [ -f docs/_build/latex/smoke-test.pdf ]
+        image_test_cmd "make --directory=docs latexpdf LATEXMKOPTS='-silent'"
+        [ -f docs/_build/latex/smoke-test.pdf ]
     fi
 }
 
 main()
 {
-  case "${COMMAND}" in
-    build) image_build ;;
-    push) image_push ;;
-    save) image_save ;;
-    test) image_test ;;
-    *) echo "${USAGE}" && exit 1 ;;
-  esac
+    case "${COMMAND}" in
+        build) image_build ;;
+        push) image_push ;;
+        save) image_save ;;
+        test) image_test ;;
+        *) echo "${USAGE}" && exit 1 ;;
+    esac
 }
 
 main
