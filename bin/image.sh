b18d1784560b4cdba87a557c566ef5c4605875a1
Markus Zapke-Gründemann <markus@transcode.de>
2018-09-22 01:17:50 +0200

    image.sh: Refactor script
    - Add set_image_variables() to remove duplicated code.
    - Change all global readonly variables to lowercase.

--- bin/image.sh
+++ bin/image.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 # This file:
 #
 #  - Various commands for Docker images
@@ -12,11 +12,11 @@ set -e
 # Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump |gzip`
 set -o pipefail
 
-readonly BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-readonly COMMAND=$1
-readonly IMAGE=$2
-readonly TAG=$3
-readonly USAGE="Usage: $(basename "$0") [COMMAND] [IMAGE] [TAG]
+readonly build_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+readonly cmd=$1
+readonly image=$2
+readonly tag=$3
+readonly usage="Usage: $(basename "$0") [COMMAND] [IMAGE] [TAG]
 
     Available commands:
         build
@@ -25,95 +25,85 @@ readonly USAGE="Usage: $(basename "$0") [COMMAND] [IMAGE] [TAG]
         save
         test
 "
-readonly VCS_REF=$(git rev-parse --short HEAD)
-readonly VERSION=$(grep ^sphinx== requirements.pip | tr -s '==' | cut -d '=' -f 2)
+readonly vcs_ref=$(git rev-parse --short HEAD)
+readonly version=$(grep ^sphinx== requirements.pip | tr -s '==' | cut -d '=' -f 2)
 
-image_build()
-{
-    case "${TAG}" in
+set_image_variables() {
+    case "${tag}" in
         latest)
-            docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
-                --build-arg VCS_REF="${VCS_REF}" \
-                --build-arg VERSION="${VERSION}" \
-                --tag "${IMAGE}:${VERSION}" \
-                --tag "${IMAGE}:${TAG}" \
-                .
+            ext=""
+            version_tag=$version
             ;;
-        latex)
-            docker build --build-arg BUILD_DATE="${BUILD_DATE}" \
-                --build-arg VCS_REF="${VCS_REF}" \
-                --build-arg VERSION="${VERSION}" \
-                --file Dockerfile."${TAG}" \
-                --tag "${IMAGE}:${VERSION}-${TAG}" \
-                --tag "${IMAGE}:${TAG}" \
-                .
+        *)
+            ext=".${tag}"
+            version_tag="${version}-${tag}"
             ;;
     esac
 }
 
-image_push()
-{
-    case "${TAG}" in
-        latest) docker push "${IMAGE}:${VERSION}" ;;
-        latex) docker push "${IMAGE}:${VERSION}-${TAG}" ;;
-    esac
-    docker push "${IMAGE}:${TAG}"
+image_build() {
+    set_image_variables
+    docker build --build-arg BUILD_DATE="${build_date}" \
+        --build-arg VCS_REF="${vcs_ref}" \
+        --build-arg VERSION="${version}" \
+        --file Dockerfile"${ext}" \
+        --tag "${image}:${version_tag}" \
+        --tag "${image}:${tag}" \
+        .
 }
 
-image_save()
-{
-    case "${TAG}" in
-        latest) docker save "${IMAGE}:${VERSION}" "${IMAGE}:${TAG}" ;;
-        latex) docker save "${IMAGE}:${VERSION}-${TAG}" "${IMAGE}:${TAG}" ;;
-    esac
+image_push() {
+    set_image_variables
+    docker push "${image}:${version_tag}"
+    docker push "${image}:${tag}"
 }
 
-image_test_cmd()
-{
+image_save() {
+    set_image_variables
+    docker save "${image}:${version_tag}" "${image}:${tag}"
+}
+
+image_test_cmd() {
     rm -fr docs
-    cmd="sphinx-quickstart --author=me --project=smoke-test --quiet docs && $1"
-    docker run --interactive --name sphinx-doc_test --tty "${IMAGE}:${TAG}" sh -c "${cmd}"
-    container=$(docker ps --all --filter ancestor="${IMAGE}:${TAG}" --format "{{.Names}}")
+    test_cmd="sphinx-quickstart --author=me --project=smoke-test --quiet docs && $1"
+    docker run --interactive --name sphinx-doc_test --tty "${image}:${tag}" sh -c "${test_cmd}"
+    container=$(docker ps --all --filter ancestor="${image}:${tag}" --format "{{.Names}}")
     docker cp "${container}:/home/python/docs" "$(pwd)"
     docker rm "${container}"
 }
 
-image_test()
-{
+image_test() {
     image_test_cmd "make --directory=docs html"
     [ -f docs/conf.py ] && [ -f docs/Makefile ] && [ -f docs/_build/html/index.html ]
-    if [ "${TAG}" = "latex" ]; then
+    if [ "${tag}" = "latex" ]; then
         image_test_cmd "make --directory=docs latexpdf LATEXMKOPTS='-silent'"
         [ -f docs/_build/latex/smoke-test.pdf ]
     fi
 }
 
-image_release()
-{
-    echo "Are you sure you want to release version $VERSION? [y/N]"
-    read -r ANSWER
-    if echo "$ANSWER" | grep -iq "^y"; then
+image_release() {
+    echo "Are you sure you want to release version $version? [y/N]"
+    read -r answer
+    if echo "$answer" | grep -iq "^y"; then
         git diff --cached --quiet
         git checkout develop
         git pull --ff-only
         git checkout master
         git pull --ff-only
         git merge develop
-        git tag "$VERSION"
-        git push
+        git tag "$version"
         git push --tags
     fi
 }
 
-main()
-{
-    case "${COMMAND}" in
+main() {
+    case "${cmd}" in
         build) image_build ;;
         push) image_push ;;
         release) image_release ;;
         save) image_save ;;
         test) image_test ;;
-        *) echo "${USAGE}" && exit 1 ;;
+        *) echo "${usage}" && exit 1 ;;
     esac
 }
 
